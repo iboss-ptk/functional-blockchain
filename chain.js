fd689058c0d88ecfa4a0834e1147ccf14f87dc4c
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2020-07-27 20:17:02 +0700

    Validate chain

---
+++ chain.js
@@ -0,0 +1,21 @@
+const { calculateHash } = require("./hash")
+
+function validateChain(chain) {
+    function tce(chain, index) {
+        if (index === 0) return true
+        const { hash, ...currentBlockWithoutHash} = chain[index]
+        const currentBlock = chain[index]
+        const previousBlock = chain[index - 1]
+        const isValidHash = (hash === calculateHash(currentBlockWithoutHash))
+        const isPreviousHashValid = (currentBlock.previousHash === previousBlock.hash)
+        const isValidChain = (isValidHash && isPreviousHashValid)
+
+        if (!isValidChain) return false
+        else return tce(chain, index - 1)
+    }
+    return tce(chain, chain.length - 1)
+}
+
+module.exports = {
+    validateChain
+}
--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const { generateGenesisBlock ,addBlockWithConstraint, constraints } = require('./block')
+const { validateChain } = require('./chain')
 
 const addBlock = addBlockWithConstraint(constraints.padZeros(4))
 const chain = [generateGenesisBlock()]
 
-const newChain = addBlock(chain, "next block")
+console.log("=== GENESIS ===")
+console.log(chain)
+console.log("is genesis chain valid? :", validateChain(chain))
+console.log("\n")
 
+const newChain = addBlock(chain, {
+    sender:   "ks829fh28192j28d9dk9",
+    receiver: "ads8d91w29jsm2822910",
+    amount:   0.0023,
+    currency: "BTC"
+})
+
+
+console.log("=== UPDATED ===")
 console.log(newChain)
+console.log("is properly updated chain valid? : ", validateChain(newChain))
+console.log("\n")
+
+const contaminatedChain = [
+    ...newChain,
+    {
+        timestamp: + new Date(),
+        data: "Genesis Block",
+        previousHash: newChain[newChain.length - 1].hash,
+        hash: "invalidhash"
+    }
+]
+
+console.log("=== CONTAMINATED ===")
+console.log(contaminatedChain)
+console.log("is contaminated chain valid? : ", validateChain(contaminatedChain))
